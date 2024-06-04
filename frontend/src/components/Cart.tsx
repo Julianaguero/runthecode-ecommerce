7df95c48589b3f3354290234c0ee9fd93fb60507
Julian <julianaguero@gmail.com>
2024-06-05 01:21:25 +0200

    add package.json

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CartButton, CartItemsList, LinkButtonActive } from ".";
 import { useCart } from "../hooks";
 import { calculateTotalPrice } from "../utils/cartUtils";
