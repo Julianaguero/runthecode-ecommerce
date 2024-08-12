c1cb15b4adfdc4106e62caa6bb9fdca628741c29
Julian <julianaguero@gmail.com>
2024-08-12 10:33:06 +0200

    test: create a new BASE_URL to fix filter API calls

--- frontend/src/services/getFilterProducts.ts
+++ frontend/src/services/getFilterProducts.ts
@@ -3,12 +3,18 @@ import { errorHandler } from "../utils/errorHandler";
 
 
 const getFilterProducts = async ({ brand, minPrice, maxPrice }: FilterProps): Promise<ProductsProps[] | Error> => {
-    const url = new URL(`${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : '/'}api/filter`)
+    const BASE_URL = import.meta.env.VITE_API_URL
+
+    // const url = new URL(`${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : '/'}api/filter`)
+    console.log(BASE_URL, "BASE_URL")
+
+    const url = new URL('/api/filter', BASE_URL)
     if (brand) brand.map(singleBrand => (
         url.searchParams.append("brand", singleBrand)
     ));
     if(minPrice && minPrice > 0) url.searchParams.append("minPrice", String(minPrice))
     if(maxPrice && maxPrice < 2000) url.searchParams.append("maxPrice", String(maxPrice))
+    console.log(url)
 
     try {
         const response = await fetch(url, {
